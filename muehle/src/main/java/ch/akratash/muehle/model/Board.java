3f2169ed7ffadc1baa0c3ed25ab74476074f0db1
akratash <patrickbuehler@hotmail.de>
2020-09-28 21:38:10 +0200

    Init muehle

---
+++ muehle/src/main/java/ch/akratash/muehle/model/Board.java
@@ -0,0 +1,13 @@
+package ch.akratash.muehle.model;
+
+/**
+ * Hello world!
+ *
+ */
+public class Board
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ muehle/src/test/java/ch/akratash/muehle/model/BoardTest.java
@@ -0,0 +1,18 @@
+package ch.akratash.muehle.model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+public class BoardTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
