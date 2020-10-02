ac439b686b84c014dda48269c746ea6c4766ef79
Akratash <patrickbuehler@hotmail.de>
2020-10-02 11:03:27 +0200

    update .classpath, GUI.java

--- muehle/src/main/java/ch/akratash/muehle/view/Gui.java
+++ muehle/src/main/java/ch/akratash/muehle/view/Gui.java
@@ -1,11 +1,11 @@
 package ch.akratash.muehle.view;
 
-import org.graalvm.compiler.phases.common.NodeCounterPhase.Stage;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 public class GUI extends Application {
 	/**
